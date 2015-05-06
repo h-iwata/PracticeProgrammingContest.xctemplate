11031370976da2e9c2ec534f8480e952947f1eaf
h-iwata <hiwata@autoc-one.jp>
2015-05-06 15:55:08 +0900

    add files

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include "main.h"
+
+void init() {
+
+}
+
+int solve(){
+    return 0;
+}
+
+
+int main(int argc, const char * argv[]) {
+    cout << "input values : " << endl;
+    init();
+    cout << solve();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
